240aa53bcc3ae7923a905b5cbda4de1c5dbc37db
Jwiroo <qorudgus77@gmail.com>
2025-08-21 18:08:34 +0900

    Updates

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-import initAnimations from '/portfolio/animations/index.js';
-import initInteractions from '/portfolio/interactions/index.js';
-import initComponents from '/portfolio/components/index.js';
+import initAnimations from '/portfolio/js/animations/index.js';
+import initInteractions from '/portfolio/js/interactions/index.js';
+import initComponents from '/portfolio/js/components/index.js';
 
 initAnimations();
 initInteractions();
